a071345c6f94bc9b282ae62d07756fe3180d8e2d
FerSteban <Ferhdez0918esteban@gmail.com>
2023-11-20 20:01:45 -0600

    fase monitoreo

--- src/Rollbar.tsx
+++ src/Rollbar.tsx
@@ -1,7 +1,7 @@
 import Rollbar from 'rollbar';
 
 const rollbarConfig = {
-    accessToken: '1d88de51167544d1957c05dc46c79a18',
+    accessToken: 'd5c419a929244d08a86978e65f63e4d0',
     environment: 'testenv',
     revision: process.env.GITHUB_SHA,
 };
